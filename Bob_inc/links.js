dd21e23cbaa66ee7970453e82ac6affa61d60fe0
digitaladv1 <digital1@digitaleducationadventures.com>
2019-06-07 16:57:00 -0500

    1st commit

---
+++ Bob_inc/links.js
@@ -0,0 +1,10 @@
+var text =    "<li><a href='chat.html'> bob chat  </a>  </li>";
+text+=     "<li><a href='store.html'> bob store  </a>  </li>";
+text+=     "<li><a href='games.html'> bob games  </a>  </li>";
+text+=     "<li><a href='debate.html'> bob debates  </a>  </li>";
+text+=     "<li><a href='home.html'> bob home </a>  </li>";
+
+
+var thing=document.getElementById('links');
+
+thing.innerHTML = text;
